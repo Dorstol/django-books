440a3b5a751c44987cfa36b39e719dc7fbc47889
Dorstol <olehsofronov@gmail.com>
2023-04-02 19:17:08 +0300

    added Reviews and tests

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -7,7 +8,7 @@ from django.urls import reverse
 class Book(models.Model):
     id = models.UUIDField(
         primary_key=True,
-        default=uuid.uuid64,
+        default=uuid.uuid4,
         editable=False
     )
     title = models.CharField(max_length=200)
@@ -19,3 +20,19 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[str(self.id)])
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.review
--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,47 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from books.models import Book, Review
+
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="reviewuser",
+            email="reviewuser@mail.com",
+            password="passtest111"
+        )
+
+        cls.book = Book.objects.create(
+            title="Witcher",
+            author="Andjey Sapkovski",
+            price="50.00"
+        )
+
+        cls.review = Review.objects.create(
+            book=cls.book,
+            author=cls.user,
+            review="Wonderful book"
+        )
+
+    def test_list_book(self):
+        self.assertEqual(f"{self.book.title}", "Witcher")
+        self.assertEqual(f"{self.book.author}", "Andjey Sapkovski")
+        self.assertEqual(f"{self.book.price}", "50.00")
+
+    def test_book_listview(self):
+        responce = self.client.get(reverse("book_list"))
+        self.assertEqual(responce.status_code, 200)
+        self.assertContains(responce, "Witcher")
+        self.assertTemplateUsed(responce, "books/book_list.html")
+
+    def test_book_detail_view(self):
+        responce = self.client.get(self.book.get_absolute_url())
+        no_responce = self.client.get("/books/1488")
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(no_responce.status_code, 404)
+        self.assertContains(responce, "Witcher")
+        self.assertContains(responce, "Wonderful book")
+        self.assertTemplateUsed(responce, "books/book_detail.html")
