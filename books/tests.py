8a494bd73d978da62cdb9db186699d948584822d
Dorstol <olehsofronov@gmail.com>
2023-04-02 20:35:31 +0300

    added media and permissions

--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import TestCase
 from django.urls import reverse
 
@@ -12,6 +13,10 @@ class BookTests(TestCase):
             username="reviewuser",
             email="reviewuser@mail.com",
             password="passtest111"
+        )
+
+        cls.special_permission = Permission.objects.get(
+            codename="special_status"
         )
 
         cls.book = Book.objects.create(
@@ -31,13 +36,21 @@ class BookTests(TestCase):
         self.assertEqual(f"{self.book.author}", "Andjey Sapkovski")
         self.assertEqual(f"{self.book.price}", "50.00")
 
-    def test_book_listview(self):
+    def test_book_listview_for_logged_in_user(self):
+        self.client.login(email="reviewuser@mail.com", password="passtest111")
         responce = self.client.get(reverse("book_list"))
         self.assertEqual(responce.status_code, 200)
         self.assertContains(responce, "Witcher")
         self.assertTemplateUsed(responce, "books/book_list.html")
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_unlogged_user(self):
+        self.client.logout()
+        responce = self.client.get(reverse("book_list"))
+        self.assertEqual(responce.status_code, 302)
+
+    def test_book_detail_view_with_permission(self):
+        self.client.login(email="reviewuser@mail.com", password="passtest111")
+        self.user.user_permissions.add(self.special_permission)
         responce = self.client.get(self.book.get_absolute_url())
         no_responce = self.client.get("/books/1488")
         self.assertEqual(responce.status_code, 200)
